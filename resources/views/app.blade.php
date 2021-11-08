708818a27b372b71c32bcb8ff5d971aea4da8df0
LINARDS L <linards_lazdins@hotmail.com>
2021-11-08 19:24:59 +0200

    intial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Modern House</title>
+
+        <!-- Fonts -->
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+    </head>
+    <body class="antialiased">
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('app')
+@section('content')
+    <h1>Welcome!</h1>
+@endsection
