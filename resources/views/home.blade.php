0d4c6d75aec706b953736e883aa19bd21bb27ce9
LINARDS L <linards_lazdins@hotmail.com>
2021-11-23 20:44:49 +0200

    further development for product page template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
           <h1 class="fw-bold text-center text-uppercase">Model 1</h1>
         </div>
         <div class="order-now text-center d-flex flex-column justify-content-end align-items-center h-100">
-          <a href="/product" class="btn btn-primary btn-order-now fw-light d-flex justify-content-center align-items-center">@lang('customer order')</a>
+          <a href="/product" class="btn btn-primary btn-main fw-light d-flex justify-content-center align-items-center">@lang('customer order')</a>
           <a href="#model-two" class="pb-lg-5 pb-4 pt-3">
             <img width="35" height="35" src="{{ asset('storage/arrow-down.svg') }}" alt="Arrow down">
           </a>
@@ -22,7 +22,7 @@
           <h1 class="fw-bold text-center text-uppercase">Model 2</h1>
         </div>
         <div class="order-now text-center d-flex flex-column justify-content-end align-items-center h-100">
-          <a href="#" class="btn btn-primary btn-order-now fw-light d-flex justify-content-center align-items-center">@lang('customer order')</a>
+          <a href="#" class="btn btn-primary btn-main fw-light d-flex justify-content-center align-items-center">@lang('customer order')</a>
           <a href="#model-three" class="pb-lg-5 pb-4 pt-3">
             <img width="35" height="35" src="{{ asset('storage/arrow-down.svg') }}" alt="Arrow down">
           </a>
@@ -35,7 +35,7 @@
           <h1 class="fw-bold text-center text-uppercase">Model 3</h1>
         </div>
         <div class="order-now text-center d-flex flex-column justify-content-end align-items-center h-100">
-          <a href="#" class="btn btn-primary btn-order-now fw-light d-flex justify-content-center align-items-center">@lang('customer order')</a>
+          <a href="#" class="btn btn-primary btn-main fw-light d-flex justify-content-center align-items-center">@lang('customer order')</a>
           <a href="#model-two" class="pb-lg-5 pb-4 pt-3">
             <img width="35" height="35" class="arrow-up" src="{{ asset('storage/arrow-down.svg') }}" alt="Arrow down">
           </a>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,47 +2,76 @@
 @section('content')
   @include('includes.navbar-desktop', ['index' => false])
   @include('includes.navbar-mobile', ['index' => false])
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-7"></div>
-      <div class="col-lg-5">
-        <div class="title">
-          <h1 class="fw-bold text-center text-uppercase">Model 1</h1>
-        </div>
-        <div class="house-trims">
-          <ul class="nav nav-tabs d-flex justify-content-between house-trim-titles pb-4">
-            <li class="nav-item">
-              <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#premium" type="button">Premium</button>
-            </li>
-            <li class="nav-item">
-              <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#standart" type="button">Standart</button>
-            </li>
-            <li class="nav-item">
-              <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#economy" type="button">Economy</button>
-            </li>
-          </ul>
-          <div class="tab-content house-trim-content">
-            <div class="tab-pane show active" id="premium">
-              <h1 class="text-center pb-4">EUR 15 000,00</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maxime earum cum dolorum, unde molestias eligendi debitis rem esse consequuntur
-                odit
-                alias voluptas dolor laudantium sit! Deserunt cum odio et?</p>
+  <form>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-7"></div>
+        <div class="col-lg-5">
+          <div class="title">
+            <h1 class="fw-bold text-center text-uppercase">Model 1</h1>
+          </div>
+          <div class="house-trims">
+            <ul class="nav nav-tabs d-flex justify-content-between house-trim-titles">
+              <li class="nav-item">
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#premium" type="button">Premium</button>
+              </li>
+              <li class="nav-item">
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#standart" type="button">Standart</button>
+              </li>
+              <li class="nav-item">
+                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#economy" type="button">Economy</button>
+              </li>
+            </ul>
+            <div class="tab-content house-trim-content">
+              <div class="tab-pane show active" id="premium">
+                <div class="price">
+                  <h1 class="text-center">EUR 15 000,00</h1>
+                </div>
+                <div class="description">
+                  <p class="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maxime earum cum dolorum, unde molestias eligendi
+                    debitis
+                    rem esse consequuntur
+                    odit
+                    alias voluptas dolor laudantium sit! Deserunt cum odio et?</p>
+                </div>
+                <div class="feature-details-btn text-center d-flex flex-column justify-content-end align-items-center h-100">
+                  <a href="#" class="btn btn-primary btn-main fw-light d-flex justify-content-center align-items-center">@lang('feature details')</a>
+                </div>
+                <div class="available-options mt-5">
+                  <div class="windows d-flex justify-content-center align-items-center flex-column">
+                    <h4 class="fw-bold text-center">Windows</h4>
+                    <div class="options d-flex justify-content-center align-items-center">
+                      <div class="m-3 option form-check">
+                        <input class="form-check-input option-icon" type="radio" name="flexRadioDefault" id="" checked>
+                        <label class="form-check-label text-center" for="flexRadioDefault1">
+                          Solid Black Oak
+                        </label>
+                      </div>
+                      <div class="m-3 option form-check">
+                        <input class="form-check-input option-icon" type="radio" name="flexRadioDefault" id="">
+                        <label class="form-check-label text-center" for="flexRadioDefault2">
+                          Solid Black Oak
+                        </label>
+                      </div>
+                      <div class="m-3 option form-check">
+                        <input class="form-check-input option-icon" type="radio" name="flexRadioDefault" id="">
+                        <label class="form-check-label text-center" for="flexRadioDefault2">
+                          Solid Black Oak
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div class="tab-pane" id="standart">
-              <h2>EUR 15000,00</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maxime earum cum dolorum, unde molestias eligendi debitis rem esse consequuntur
-                odit
-                alias voluptas dolor laudantium sit! Deserunt cum odio et?</p>
             </div>
             <div class="tab-pane" id="economy">
-              <h2>EUR 15000,00</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maxime earum cum dolorum, unde molestias eligendi debitis rem esse consequuntur
-                odit
-                alias voluptas dolor laudantium sit! Deserunt cum odio et?</p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+    </div>
+  </form>
 @endsection
