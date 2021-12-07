4211755b6e1ef7f8b407d9ab2c89589ecfdb015b
LINARDS L <linards_lazdins@hotmail.com>
2021-12-07 20:14:01 +0200

    product page configuration side template

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,7 +4,7 @@
   @include('includes.navbar-mobile', ['index' => false])
   <form method="POST" action="{{ route('submit_order') }}" enctype="multipart/form-data">
     @csrf
-    <div class="container-fluid">
+    <div class="container-fluid content">
       <div class="row">
         <div class="col-lg-7"></div>
         <div class="col-lg-5">
@@ -12,7 +12,7 @@
             <h1 class="fw-bold text-center text-uppercase">Model 1</h1>
           </div>
           <div class="product-levels">
-            <ul class="nav nav-tabs d-flex justify-content-evenly product-level-titles flex-nowrap">
+            <ul class="nav my-4 nav-tabs d-flex product-level-titles flex-nowrap">
               <li class="nav-item">
                 <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#premium" type="button">Premium</button>
               </li>
@@ -35,36 +35,36 @@
                     odit
                     alias voluptas dolor laudantium sit! Deserunt cum odio et?</p>
                 </div>
-                <div class="product-description-btn text-center d-flex flex-column justify-content-end align-items-center h-100">
+                <div class="product-description-btn mt-4 text-center d-flex flex-column justify-content-end align-items-center h-100">
                   <a href="#"
                     class="btn btn-primary btn-main btn-secondary fw-light d-flex justify-content-center align-items-center text-uppercase">@lang('feature
                     details')</a>
                 </div>
                 <div class="product-options">
-                  <div class="product-option d-flex justify-content-center align-items-center flex-column">
+                  <div class="product-option my-5 d-flex justify-content-center align-items-center flex-column">
                     <h4 class="fw-bold text-center">Windows</h4>
                     <div class="options d-flex justify-content-center align-items-center">
                       <div class="m-3 option form-check">
-                        <input class="form-check-input option-icon" type="radio" name="flexRadioDefault" id="" checked>
-                        <label class="form-check-label text-center" for="flexRadioDefault">
+                        <input class="form-check-input option-icon mb-2" type="radio" name="flexRadioDefault" id="" checked>
+                        <label class="form-check-label text-center option-label" for="flexRadioDefault">
                           Solid Black Oak
                         </label>
                       </div>
                       <div class="m-3 option form-check">
-                        <input class="form-check-input option-icon" type="radio" name="flexRadioDefault" id="">
-                        <label class="form-check-label text-center" for="flexRadioDefault">
+                        <input class="form-check-input option-icon mb-2" type="radio" name="flexRadioDefault" id="">
+                        <label class="form-check-label text-center option-label" for="flexRadioDefault">
                           Solid Black Oak
                         </label>
                       </div>
                       <div class="m-3 option form-check">
-                        <input class="form-check-input option-icon" type="radio" name="flexRadioDefault" id="">
-                        <label class="form-check-label text-center" for="flexRadioDefault">
+                        <input class="form-check-input option-icon mb-2" type="radio" name="flexRadioDefault" id="">
+                        <label class="form-check-label text-center option-label" for="flexRadioDefault">
                           Solid Black Oak
                         </label>
                       </div>
                     </div>
                   </div>
-                  <div class="product-option d-flex justify-content-center align-items-center flex-column">
+                  <div class="product-option my-5 d-flex justify-content-center align-items-center flex-column">
                     <h4 class="fw-bold text-center">Facade</h4>
                     <div class="options d-flex justify-content-center align-items-center">
                       <div class="m-3 option form-check">
@@ -87,7 +87,7 @@
                       </div>
                     </div>
                   </div>
-                  <div class="product-option d-flex justify-content-center align-items-center flex-column">
+                  <div class="product-option my-5 d-flex justify-content-center align-items-center flex-column">
                     <h4 class="fw-bold text-center">Roof</h4>
                     <div class="options d-flex justify-content-center align-items-center">
                       <div class="m-3 option form-check">
@@ -118,7 +118,7 @@
               </div>
             </div>
             <div class="product-order d-flex flex-column justify-content-end align-items-center h-100">
-              <h4 class="fw-bold text-center">Order your Model 1</h4>
+              <h4 class="fw-bold text-center mb-2">Order your Model 1</h4>
               <button type="submit"
                 class="btn btn-primary btn-main btn-secondary fw-light d-flex justify-content-center align-items-center text-uppercase">@lang('order now')</button>
             </div>
