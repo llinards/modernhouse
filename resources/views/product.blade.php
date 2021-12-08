7e159d8fa7bcc44f063d5c3a7f43977ff9b813d2
LINARDS L <linards_lazdins@hotmail.com>
2021-12-08 21:58:38 +0200

    gallery template created

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -7,17 +7,25 @@
     <div class="container-fluid content">
       <div class="row">
         <div class="col-lg-7">
-          <div class="main-img">
-            <img src="{{ asset('storage/model-1/1.jpg') }}" alt="" id="current">
+          <div class="product-thumbnail d-flex">
+            <div class="arrow-left d-flex align-items-center m-2">
+              <img width="35" height="35" class="invisible" id="previous-photo" src="{{ asset('storage/arrow-down-black.svg') }}" alt="">
+            </div>
+            <div class="thumbnail-img">
+              <img class="img-fluid" src="{{ asset('storage/model-1/1.jpg') }}" alt="" id="current-img">
+            </div>
+            <div class="arrow-right d-flex align-items-center m-2">
+              <img width="35" height="35" id="next-photo" src="{{ asset('storage/arrow-down-black.svg') }}" alt="">
+            </div>
           </div>
-          <div class="imgs">
-            <img src="{{ asset('storage/model-1/1.jpg') }}" alt="">
-            <img src="{{ asset('storage/model-1/2.jpg') }}" alt="">
-            <img src="{{ asset('storage/model-1/3.jpg') }}" alt="">
-            <img src="{{ asset('storage/model-1/4.jpg') }}" alt="">
-            <img src="{{ asset('storage/model-1/5.jpg') }}" alt="">
-            <img src="{{ asset('storage/model-1/6.jpg') }}" alt="">
-            <img src="{{ asset('storage/model-1/7.jpg') }}" alt="">
+          <div class="product-imgs">
+            <img class="img-fluid" src="{{ asset('storage/model-1/1.jpg') }}" alt="">
+            <img class="img-fluid" src="{{ asset('storage/model-1/2.jpg') }}" alt="">
+            <img class="img-fluid" src="{{ asset('storage/model-1/3.jpg') }}" alt="">
+            <img class="img-fluid" src="{{ asset('storage/model-1/4.jpg') }}" alt="">
+            <img class="img-fluid" src="{{ asset('storage/model-1/5.jpg') }}" alt="">
+            <img class="img-fluid" src="{{ asset('storage/model-1/6.jpg') }}" alt="">
+            <img class="img-fluid" src="{{ asset('storage/model-1/7.jpg') }}" alt="">
           </div>
         </div>
         <div class="col-lg-5">
@@ -142,22 +150,59 @@
     </div>
   </form>
   <script>
-    const current = document.querySelector('#current');
-    const imgs = document.querySelectorAll('.imgs img');
+    const currentImg = document.getElementById('current-img');
+    let allImgs = Array.prototype.slice.call(document.querySelectorAll('.product-imgs img'));
     const opacity = 0.4;
+    const nextPhoto = document.getElementById('next-photo');
+    const previousPhoto = document.getElementById('previous-photo');
+    let currentImgIndex = 0;
 
-    imgs.forEach((img) => {
+    allImgs.forEach((img) => {
       img.addEventListener('click', (e) => {
-        imgClick(e);
+        allImgs.forEach((img) => {
+          img.style.opacity = 1;
+        });
+        currentImgIndex = allImgs.findIndex(element => element.src == e.target.src);
+        currentImg.src = e.target.src;
+        e.target.style.opacity = opacity;
+        if (currentImgIndex > 0) {
+          previousPhoto.classList.remove('invisible');
+        } else {
+          previousPhoto.classList.add('invisible');
+        }
+        if (currentImgIndex >= (allImgs.length) - 1) {
+          nextPhoto.classList.add('invisible');
+        } else {
+          nextPhoto.classList.remove('invisible');
+        }
       })
     })
 
-    function imgClick(e) {
-      imgs.forEach((img) => {
-        img.style.opacity = 1;
-      });
-      current.src = e.target.src;
-      e.target.style.opacity = opacity;
-    }
+    nextPhoto.addEventListener('click', () => {
+      currentImgIndex += 1;
+      for (let i = 0; i <= allImgs.length; i++) {
+        if (i == currentImgIndex) {
+          currentImg.src = allImgs[i].src;
+        }
+      }
+      if (currentImgIndex >= (allImgs.length) - 1) {
+        nextPhoto.classList.add('invisible');
+      }
+      previousPhoto.classList.remove('invisible');
+    });
+
+
+    previousPhoto.addEventListener('click', () => {
+      currentImgIndex -= 1;
+      for (let i = (allImgs.length - 1); i >= 0; i--) {
+        if (i == currentImgIndex) {
+          currentImg.src = allImgs[i].src;
+        }
+      }
+      if (currentImgIndex == 0) {
+        previousPhoto.classList.add('invisible');
+      }
+      nextPhoto.classList.remove('invisible');
+    });
   </script>
 @endsection
