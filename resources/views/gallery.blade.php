f4b6c867d5ce410284957f66006e0432903450fa
LINARDS L <linards_lazdins@hotmail.com>
2023-01-24 14:03:04 +0200

    Added gallery front-end

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -2,15 +2,15 @@
 @section('content')
   <div class="container-xxl mb-4">
     <div class="row">
-      <h1 class="fw-bold text-center text-uppercase title">@lang('about')</h1>
-      <div class="mt-4">
+      <h1 class="fw-bold text-center text-uppercase title">@lang('gallery')</h1>
+      <div class="mt-4 gallery">
         @foreach($galleryContent as $galleryItem)
         <div class="row mt-5">
-          <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-4">
+          <div class="col-lg-3 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-2">
             <h2 class="fw-bold title mb-2">{{ $galleryItem->title }}</h2>
             <p>{!! $galleryItem->content !!}</p>
           </div>
-          <div class="col-lg-6 d-flex order-first order-lg-last justify-content-center align-items-center flex-column" id="about-us-gallery">
+          <div class="col-lg-9 d-flex order-first order-lg-last justify-content-center align-items-center flex-column" id="gallery-images">
             <section id="{{Str::slug($galleryItem->title)}}-main-carousel" class="splide">
               <div class="splide__track">
                 <ul class="splide__list">
