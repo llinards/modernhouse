3f07af3ba1f04cbc4f122a2b0522d714b15a5830
LINARDS L <linards_lazdins@hotmail.com>
2023-03-29 13:35:43 +0300

    Disable all seeders except the latest one

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,18 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-          ProductSeeder::class,
-          ProductVariantSeeder::class,
-          ImageSeeder::class,
-          ProductVariantOptionSeeder::class,
-          UserSeeder::class,
-          ProductVariantAreaDetailSeeder::class,
-          ProductVariantDetailSeeder::class,
-          NewsContentSeeder::class,
-          NewsImageSeeder::class,
-          NewsAttachmentSeeder::class,
-          GalleryContentSeeder::class,
-          GalleryImageSeeder::class,
+//          ProductSeeder::class,
+//          ProductVariantSeeder::class,
+//          ImageSeeder::class,
+//          ProductVariantOptionSeeder::class,
+//          UserSeeder::class,
+//          ProductVariantAreaDetailSeeder::class,
+//          ProductVariantDetailSeeder::class,
+//          NewsContentSeeder::class,
+//          NewsImageSeeder::class,
+//          NewsAttachmentSeeder::class,
+//          GalleryContentSeeder::class,
+//          GalleryImageSeeder::class,
           ProductVariantDetailIcon::class
         ]);
     }
