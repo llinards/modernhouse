8b4df9485a7530166219bcb1b4001f9eae595495
LINARDS L <linards_lazdins@hotmail.com>
2023-06-06 20:55:28 +0300

    Add FB pixel, add option to edit content as html

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,7 +27,30 @@
       src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
     <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
   @endif
+  @if(App::environment('production'))
+    <meta name="facebook-domain-verification" content="6f9uyw6o900t8gvvu9fhtwmn938qse" />
+  @endif
   <script src="{{ mix('/js/app.js') }}" defer></script>
+  @if(App::environment('production'))
+    <!-- Facebook Pixel Code -->
+    <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '1323535588541765');
+      fbq('track', 'PageView');
+    </script>
+    <noscript>
+      <img height="1" width="1"
+           src="https://www.facebook.com/tr?id=1323535588541765&ev=PageView&noscript=1"/>
+    </noscript>
+  @endif
 </head>
 <body class="antialiased" oncontextmenu="return false">
 @if(isset($index))
@@ -36,5 +59,6 @@
 <div class="content w-100 h-100">
   @yield('content')
 </div>
+<script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=RFwwRz"></script>
 </body>
 </html>
