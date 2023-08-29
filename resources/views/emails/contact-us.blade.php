3429d1e32139f07d6b3c39beb22f3b29e82a1ba4
LINARDS L <linards_lazdins@hotmail.com>
2023-08-29 22:03:31 +0300

    Update contact us form

--- resources/views/emails/contact-us.blade.php
+++ resources/views/emails/contact-us.blade.php
@@ -1,6 +1,7 @@
 <h4>Automātiskais e-pasts no Modern House</h4>
 <p>Ir pienākusi jauna ziņa.</p>
-<p>Vārds, uzvārds: <strong>{{ $data['name-surname'] }}</strong></p>
+<p>Vārds: <strong>{{ $data['first-name'] }}</strong></p>
+<p>Uzvārds: <strong>{{ $data['last-name'] }}</strong></p>
 <p>E-pasts: <strong>{{ $data['email'] }}</strong></p>
 <p>Kontakttālrunis: <strong>{{ $data['phone-number'] }}</strong></p>
 @if (isset($data['company']))
