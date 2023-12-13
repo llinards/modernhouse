5e12fddfd4cb163bfcedb088ca49c54bd76f5a91
Linards <linards_lazdins@hotmail.com>
2023-12-13 12:37:53 +0200

    Update ProductVariant controller query

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,32 +6,32 @@
       @include('includes.status-messages')
       <div>
         <ul class="nav mt-4 nav-tabs d-flex product-variant-titles">
-          @if(count($product->productVariants) !== 1)
-            @foreach($product->productVariants as $variant)
+          @if(count($productVariants) !== 1)
+            @foreach($productVariants as $productVariant)
               <li class="nav-item">
                 <button class="nav-link {{ $loop->first ? 'active' : '' }}" data-bs-toggle="tab"
-                        data-bs-target="#{{$variant->slug}}"
-                        type="button">{{ $variant->translations[0]->name}}</button>
+                        data-bs-target="#{{$productVariant->slug}}"
+                        type="button">{{ $productVariant->translations[0]->name}}</button>
               </li>
             @endforeach
           @endif
         </ul>
         <div class="tab-content">
-          @foreach($product->productVariants as $variant)
-            <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="{{$variant->slug}}">
+          @foreach($productVariants as $productVariant)
+            <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="{{$productVariant->slug}}">
               <div class="row">
                 <div class="col-lg-7 mt-4">
-                  <section id="{{$variant->slug}}-main-carousel" class="splide">
+                  <section id="{{$productVariant->slug}}-main-carousel" class="splide">
                     <div class="splide__track">
                       <ul class="splide__list">
-                        @foreach($variant->productVariantImages as $image)
-                          @if($image->product_variant_id === $variant->id)
+                        @foreach($productVariant->productVariantImages as $image)
+                          @if($image->product_variant_id === $productVariant->id)
                             <li class="splide__slide">
-                              <a data-fslightbox="{{$variant->slug}}"
-                                 href="{{ asset('storage/product-images/'.$product->slug.'/'.$variant->slug.'/'.$image->filename) }}">
+                              <a data-fslightbox="{{$productVariant->slug}}"
+                                 href="{{ asset('storage/product-images/'.$product->slug.'/'.$productVariant->slug.'/'.$image->filename) }}">
                                 <img class="img-fluid"
-                                     data-splide-lazy="{{ asset('storage/product-images/'.$product->slug.'/'.$variant->slug.'/'.$image->filename) }}"
-                                     alt="{{ $variant->{'name_'.app()->getLocale()} }}">
+                                     data-splide-lazy="{{ asset('storage/product-images/'.$product->slug.'/'.$productVariant->slug.'/'.$image->filename) }}"
+                                     alt="{{ $productVariant->{'name_'.app()->getLocale()} }}">
                               </a>
                             </li>
                           @endif
@@ -39,15 +39,15 @@
                       </ul>
                     </div>
                   </section>
-                  <section id="{{$variant->slug}}-thumbnail-carousel" class="splide mt-2">
+                  <section id="{{$productVariant->slug}}-thumbnail-carousel" class="splide mt-2">
                     <div class="splide__track">
                       <ul class="splide__list">
-                        @foreach($variant->productVariantImages as $image)
-                          @if($image->product_variant_id === $variant->id)
+                        @foreach($productVariant->productVariantImages as $image)
+                          @if($image->product_variant_id === $productVariant->id)
                             <li class="splide__slide">
                               <img class="img-fluid"
-                                   data-splide-lazy="{{ asset('storage/product-images/'.$product->slug.'/'.$variant->slug.'/'.$image->filename) }}"
-                                   alt="{{ $variant->{'name_'.app()->getLocale()} }}"/>
+                                   data-splide-lazy="{{ asset('storage/product-images/'.$product->slug.'/'.$productVariant->slug.'/'.$image->filename) }}"
+                                   alt="{{ $productVariant->{'name_'.app()->getLocale()} }}"/>
                             </li>
                           @endif
                         @endforeach
@@ -61,60 +61,58 @@
                     <ul class="nav nav-tabs d-flex product-variant-option-titles flex-nowrap">
                       <li class="nav-item">
                         <button class="nav-link active basic-variant-title" data-bs-toggle="tab"
-                                data-bs-target="#basic-{{$variant->slug}}" type="button">@lang('basic')
+                                data-bs-target="#basic-{{$productVariant->slug}}" type="button">@lang('basic')
                         </button>
                       </li>
                       <li class="nav-item">
                         <button class="nav-link full-variant-title" data-bs-toggle="tab"
-                                data-bs-target="#full-{{$variant->slug}}" type="button">@lang('full')
+                                data-bs-target="#full-{{$productVariant->slug}}" type="button">@lang('full')
                         </button>
                       </li>
                     </ul>
                     <div class="mt-4 mb-2">
                       <h2
-                        class="text-center fw-bold basic-variant-price title show active basic-{{$variant->slug}}">
-                        @if($variant->price_basic != 0.00)
-                          EUR {{ number_format($variant->price_basic, 2, ',', ' ') }}
+                        class="text-center fw-bold basic-variant-price title show active basic-{{$productVariant->slug}}">
+                        @if($productVariant->price_basic != 0.00)
+                          EUR {{ number_format($productVariant->price_basic, 2, ',', ' ') }}
                         @else
                           @lang('individual price')
                         @endif
                       </h2>
-                      <h2 class="text-center fw-bold full-variant-price title full-{{$variant->slug}}">
-                        @if($variant->price_full != 0.00)
-                          EUR {{ number_format($variant->price_full, 2, ',', ' ') }}
+                      <h2 class="text-center fw-bold full-variant-price title full-{{$productVariant->slug}}">
+                        @if($productVariant->price_full != 0.00)
+                          EUR {{ number_format($productVariant->price_full, 2, ',', ' ') }}
                         @else
                           @lang('individual price')
                         @endif
                       </h2>
                     </div>
                     <div class="product-short-description">
-                      <p>{!! $variant->translations[0]->description !!}</p>
+                      <p>{!! $productVariant->translations[0]->description !!}</p>
                       <div class="product-details">
                         <div class="product-details-header d-flex justify-content-between">
-                          <p>@lang('living space') : {{ $variant->living_area }} m<sup>2</sup></p>
-                          <p>@lang('construction area') : {{ $variant->building_area }} m<sup>2</sup></p>
+                          <p>@lang('living space') : {{ $productVariant->living_area }} m<sup>2</sup></p>
+                          <p>@lang('construction area') : {{ $productVariant->building_area }} m<sup>2</sup></p>
                         </div>
-                        @if($variant->productVariantDetails->count() > 0)
+                        @if($productVariant->productVariantDetails->count() > 0)
                           <hr class="m-1">
                           <ul class="product-details-content p-0 m-0">
-                            @foreach($variant->productVariantDetails as $productVariantDetail)
-                              @if($productVariantDetail->language === app()->getLocale())
-                                <li class="d-flex justify-content-between">
-                                  <div>
-                                    <img
-                                      src="{{ $productVariantDetail->hasThis ? asset('storage/icons/check.svg') : asset('storage/icons/negative.svg') }}"/>{{ $productVariantDetail->name }}
-                                  </div>
-                                  <div>
-                                    <img
-                                      src="{{ asset('storage/icons/product-variant-detail-icons/'.$productVariantDetail->icon.'.svg') }}"/>
-                                    @if($productVariantDetail->count === 0)
-                                      <span class="invisible">-</span>
-                                    @else
-                                      {{ $productVariantDetail->count }}
-                                    @endif
-                                  </div>
-                                </li>
-                              @endif
+                            @foreach($productVariant->productVariantDetails as $productVariantDetail)
+                              <li class="d-flex justify-content-between">
+                                <div>
+                                  <img
+                                    src="{{ $productVariantDetail->hasThis ? asset('storage/icons/check.svg') : asset('storage/icons/negative.svg') }}"/>{{ $productVariantDetail->name }}
+                                </div>
+                                <div>
+                                  <img
+                                    src="{{ asset('storage/icons/product-variant-detail-icons/'.$productVariantDetail->icon.'.svg') }}"/>
+                                  @if($productVariantDetail->count === 0)
+                                    <span class="invisible">-</span>
+                                  @else
+                                    {{ $productVariantDetail->count }}
+                                  @endif
+                                </div>
+                              </li>
                             @endforeach
                           </ul>
                           <hr class="m-1 mb-3">
@@ -128,13 +126,13 @@
                       data-bs-toggle="modal" data-bs-target="#request-product-info">@lang('customer order')</button>
                   </div>
                 </div>
-                @if(count($variant->productVariantOptions) > 0)
+                @if(count($productVariant->productVariantOptions) > 0)
                   <div class="tab-content product-variant-option mt-4">
                     <h3 class="fw-bold text-center my-2">@lang('tech specs')</h3>
-                    <div class="tab-pane fade show active" id="basic-{{$variant->slug}}">
+                    <div class="tab-pane fade show active" id="basic-{{$productVariant->slug}}">
                       <div class="accordion accordion-flush">
-                        @foreach($variant->productVariantOptions as $option)
-                          @if($option->product_variant_id === $variant->id && $option->option_category === 'Basic' && $option->language === app()->getLocale())
+                        @foreach($productVariant->productVariantOptions as $option)
+                          @if($option->product_variant_id === $productVariant->id && $option->option_category === 'Basic')
                             <div class="accordion-item">
                               <h2 class="accordion-header">
                                 <button class="accordion-button collapsed variant-button" type="button"
@@ -155,10 +153,10 @@
                         @endforeach
                       </div>
                     </div>
-                    <div class="tab-pane fade" id="full-{{$variant->slug}}">
+                    <div class="tab-pane fade" id="full-{{$productVariant->slug}}">
                       <div class="accordion accordion-flush">
-                        @foreach($variant->productVariantOptions as $option)
-                          @if($option->product_variant_id === $variant->id && $option->option_category === 'Full' && $option->language === app()->getLocale())
+                        @foreach($productVariant->productVariantOptions as $option)
+                          @if($option->product_variant_id === $productVariant->id && $option->option_category === 'Full')
                             <div class="accordion-item">
                               <h2 class="accordion-header">
                                 <button class="accordion-button collapsed variant-button" type="button"
