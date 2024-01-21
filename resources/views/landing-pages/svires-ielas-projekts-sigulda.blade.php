36d42493be0bfe6764ca2cb7aa2ab7038722ec93
Linards <9407839+llinards@users.noreply.github.com>
2024-01-21 12:09:01 +0200

    Update layout for mobile

--- resources/views/landing-pages/svires-ielas-projekts-sigulda.blade.php
+++ resources/views/landing-pages/svires-ielas-projekts-sigulda.blade.php
@@ -97,135 +97,68 @@
       </div>
     </section>
     <section id="about-project-mobile" class="d-md-none d-block">
-      <div class="container-xxl w-100 h-100 d-flex flex-column justify-content-center">
+      <div class="container-xxl w-100 h-100 d-flex flex-column justify-content-evenly">
         <div class="row">
-          <div class="d-flex flex-column justify-content-center">
-            <div class="row">
-              <div class="col-6 text-center">
-                <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/house.svg')}}"
-                     alt="">
-                <p>dzīvojamā platība</p>
-                <div class="mx-auto text-center w-75">
-                  <hr class="m-1">
-                </div>
-                <h3 class="title fw-bold">101 - 180 m<sup>2</sup></h3>
-              </div>
-              <div class="col-6 text-center">
-                <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/card.svg')}}"
-                     alt="">
-                <p>plānotās cenas</p>
-                <div class="mx-auto text-center w-75">
-                  <hr class="m-1">
-                </div>
-                <h3 class="title fw-bold">305 000 EUR</h3>
-              </div>
+          <div class="col-6 text-center">
+            <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/house.svg')}}"
+                 alt="">
+            <p>dzīvojamā platība</p>
+            <div class="mx-auto text-center w-75">
+              <hr class="m-1">
             </div>
-            <div class="row mt-lg-5 mt-4">
-              <div class="col-6 text-center">
-                <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/bed.svg')}}"
-                     alt="">
-                <p>istabas</p>
-                <div class="mx-auto text-center w-75">
-                  <hr class="m-1">
-                </div>
-                <h3 class="title fw-bold">3 guļamistabas</h3>
-              </div>
-              <div class="col-6 text-center">
-                <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/check.svg')}}"
-                     alt="">
-                <p>energoefektivitāte</p>
-                <div class="mx-auto text-center w-75">
-                  <hr class="m-1">
-                </div>
-                <h3 class="title fw-bold">A + klase</h3>
-              </div>
+            <h3 class="title fw-bold">101 - 180 m<sup>2</sup></h3>
+          </div>
+          <div class="col-6 text-center">
+            <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/card.svg')}}"
+                 alt="">
+            <p>plānotās cenas</p>
+            <div class="mx-auto text-center w-75">
+              <hr class="m-1">
             </div>
-            <div class="row mt-lg-5 mt-4">
-              <div class="col-6 text-center">
-                <img class="about-project-icon mb-lg-3 mb-2"
-                     src="{{asset('storage/landing-pages/icons/map-pin-line copy.svg')}}"
-                     alt="">
-                <div class="mx-auto text-center w-75">
-                  <hr class="m-1">
-                </div>
-                <h3 class="title fw-bold">labiekārtota teritorija</h3>
-              </div>
-              <div class="col-6 text-center">
-                <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/key.svg')}}"
-                     alt="">
-                <div class="mx-auto text-center w-75">
-                  <hr class="m-1">
-                </div>
-                <h3 class="title fw-bold">līdz atslēgai</h3>
-              </div>
+            <h3 class="title fw-bold">305 000 EUR</h3>
+          </div>
+        </div>
+        <div class="row mt-lg-5 mt-4">
+          <div class="col-6 text-center">
+            <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/bed.svg')}}"
+                 alt="">
+            <p>istabas</p>
+            <div class="mx-auto text-center w-75">
+              <hr class="m-1">
             </div>
+            <h3 class="title fw-bold">3 guļamistabas</h3>
+          </div>
+          <div class="col-6 text-center">
+            <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/check.svg')}}"
+                 alt="">
+            <p>energoefektivitāte</p>
+            <div class="mx-auto text-center w-75">
+              <hr class="m-1">
+            </div>
+            <h3 class="title fw-bold">A + klase</h3>
+          </div>
+        </div>
+        <div class="row mt-lg-5 mt-4">
+          <div class="col-6 text-center">
+            <img class="about-project-icon mb-lg-3 mb-2"
+                 src="{{asset('storage/landing-pages/icons/map-pin-line copy.svg')}}"
+                 alt="">
+            <div class="mx-auto text-center w-75">
+              <hr class="m-1">
+            </div>
+            <h3 class="title fw-bold">labiekārtota teritorija</h3>
+          </div>
+          <div class="col-6 text-center">
+            <img class="about-project-icon mb-lg-3 mb-2" src="{{asset('storage/landing-pages/icons/key.svg')}}"
+                 alt="">
+            <div class="mx-auto text-center w-75">
+              <hr class="m-1">
+            </div>
+            <h3 class="title fw-bold">līdz atslēgai</h3>
           </div>
         </div>
       </div>
     </section>
-    {{--    <section id="live-in-sigulda">--}}
-    {{--      <div class="container-xxl w-100 h-100 d-flex align-items-center">--}}
-    {{--        <div class="row">--}}
-    {{--          <div class="col-lg-4 d-flex flex-column justify-content-center mt-lg-0 mt-3">--}}
-    {{--            <h2 class="fw-bold title mb-3">Piepildi sapni par māju Siguldā!</h2>--}}
-    {{--            <p class="mb-3">Piedāvājam veikt rezervāciju vienstāvīgu privātmāju iegādei jaunā dzīvojamajā projektā--}}
-    {{--              Siguldā, Svīres--}}
-    {{--              ielā.--}}
-    {{--              Iespējams izvēlēties no 5 zemesgabaliem.</p>--}}
-    {{--            <p>600 m attālumā no Siguldas Sporta skolas, 900 m attālumā no TC "Raibais suns" un starppilsētu autobusu--}}
-    {{--              pieturas, 1.6 km attālumā no pilsētas centra</p>--}}
-    {{--          </div>--}}
-    {{--          <div class="col-lg-8 order-first order-lg-last" id="landing-page-galleries">--}}
-    {{--            <div id="svires-ielas-projekts-gallery-main-carousel" class="splide">--}}
-    {{--              <div class="splide__track">--}}
-    {{--                <ul class="splide__list">--}}
-    {{--                  <li class="splide__slide">--}}
-    {{--                    <a data-fslightbox="svires-ielas-projekts"--}}
-    {{--                       href="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/01.jpg') }}">--}}
-    {{--                      <img class="img-fluid"--}}
-    {{--                           data-splide-lazy="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/01.jpg') }}"--}}
-    {{--                           alt="Svires ielas projekts, Siguldā">--}}
-    {{--                    </a>--}}
-    {{--                  </li>--}}
-    {{--                  <li class="splide__slide">--}}
-    {{--                    <a data-fslightbox="svires-ielas-projekts"--}}
-    {{--                       href="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/02.jpg') }}">--}}
-    {{--                      <img class="img-fluid"--}}
-    {{--                           data-splide-lazy="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/02.jpg') }}"--}}
-    {{--                           alt="Svires ielas projekts, Siguldā">--}}
-    {{--                    </a>--}}
-    {{--                  </li>--}}
-    {{--                  <li class="splide__slide">--}}
-    {{--                    <a data-fslightbox="svires-ielas-projekts"--}}
-    {{--                       href="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/03.jpg') }}">--}}
-    {{--                      <img class="img-fluid"--}}
-    {{--                           data-splide-lazy="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/03.jpg') }}"--}}
-    {{--                           alt="Svires ielas projekts, Siguldā">--}}
-    {{--                    </a>--}}
-    {{--                  </li>--}}
-    {{--                  <li class="splide__slide">--}}
-    {{--                    <a data-fslightbox="svires-ielas-projekts"--}}
-    {{--                       href="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/04.jpg') }}">--}}
-    {{--                      <img class="img-fluid"--}}
-    {{--                           data-splide-lazy="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/04.jpg') }}"--}}
-    {{--                           alt="Svires ielas projekts, Siguldā">--}}
-    {{--                    </a>--}}
-    {{--                  </li>--}}
-    {{--                  <li class="splide__slide">--}}
-    {{--                    <a data-fslightbox="svires-ielas-projekts"--}}
-    {{--                       href="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/05.jpg') }}">--}}
-    {{--                      <img class="img-fluid"--}}
-    {{--                           data-splide-lazy="{{ asset('storage/landing-pages/svires-ielas-projekts-sigulda/05.jpg') }}"--}}
-    {{--                           alt="Svires ielas projekts, Siguldā">--}}
-    {{--                    </a>--}}
-    {{--                  </li>--}}
-    {{--                </ul>--}}
-    {{--              </div>--}}
-    {{--            </div>--}}
-    {{--          </div>--}}
-    {{--        </div>--}}
-    {{--      </div>--}}
-    {{--    </section>--}}
     <section>
       @include('includes.footer')
     </section>
