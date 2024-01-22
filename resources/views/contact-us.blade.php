021ced54b9067d057606d483d818d021cf00e70c
Linards <9407839+llinards@users.noreply.github.com>
2024-01-22 21:43:56 +0200

    Remove staff

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -57,18 +57,18 @@
           </div>
           <div class="col-lg-6 order-first order-lg-last d-flex justify-content-center align-items-between flex-column">
             <div class="row modern-house-staff">
-              <div class="col-6 text-center">
+              <div class="col-12 text-center">
                 <h5 class="fw-bold pb-1">@lang('helvijs')</h5>
                 <p>@lang('ceo')</p>
                 <a class="nav-link" href="mailto:helvijs@modern-house.lv">helvijs@modern-house.lv</a>
                 <p>+371 25666622</p>
               </div>
-              <div class="col-6 text-center">
-                <h5 class="fw-bold pb-1">@lang('aigars')</h5>
-                <p>@lang('sales specialist')</p>
-                <a class="nav-link" href="mailto:aigars@modern-house.lv">aigars@modern-house.lv</a>
-                <p>+371 26312075</p>
-              </div>
+              {{--              <div class="col-6 text-center">--}}
+              {{--                <h5 class="fw-bold pb-1">@lang('aigars')</h5>--}}
+              {{--                <p>@lang('sales specialist')</p>--}}
+              {{--                <a class="nav-link" href="mailto:aigars@modern-house.lv">aigars@modern-house.lv</a>--}}
+              {{--                <p>+371 26312075</p>--}}
+              {{--              </div>--}}
             </div>
             <div class="row mt-4">
               <div class="col-12 text-center">
