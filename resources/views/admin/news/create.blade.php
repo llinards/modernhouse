ae710a01dbbec89ec333a0ff0f59b28181dc970e
Linards <9407839+llinards@users.noreply.github.com>
2024-02-05 19:51:51 +0200

    Update layout for news mgmt

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,55 +1,49 @@
-@extends('layouts.app')
-@section('content')
-  <div class="container">
-    @include('includes.admin-navbar')
-    <section>
-      <div class="my-5">
-        <h2 class="text-center">Pievienot jaunu aktualitāti</h2>
-      </div>
-      <div class="my-5">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-7 col-12">
-              @include('includes.status-messages')
-              <form action="/admin/news" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="alert alert-secondary mx-0 my-2" role="alert">
-                  <p>Jaunums, ko pievienosi būs pieejams tikai {{ strtoupper(Lang::locale()) }}
-                    valodā.</p>
-                </div>
-                <div class="mb-3">
-                  <label for="news-title" class="form-label">Nosaukums</label>
-                  <input type="text" class="form-control" id="news-title"
-                         value="{{ old('news-title') }}"
-                         name="news-title">
-                </div>
-                <div class="mb-3">
-                  <label for="news-content" class="form-label">Apraksts</label>
-                  <x-description-text-area
-                    :name="'news-content'">{{ old('news-content') }}</x-description-text-area>
-                </div>
-                <div class="row mb-3">
-                  <div class="col">
-                    <label for="news-images" class="form-label">Bildes</label>
-                    <x-file-upload :name="'news-images'" :required="true"/>
-                  </div>
-                  <div class="col">
-                    <label for="news-attachments" class="form-label">Pielikumi</label>
-                    <x-file-upload :name="'news-attachments'"/>
-                  </div>
-                  <p class="small">Bildei ir jābūt .JPG, .JPEG vai .PNG formātā un pēc iespējas mazākā
-                    izmērā.</p>
-                  <p class="small">Tās var samazināt šajā lapā - <a href="https://compressor.io/"
-                                                                    target="_blank">compressor.io</a>
-                  <p class="small">Pielikumam ir jābūt .PDF un pēc iespējas mazākā izmērā.</p>
-                </div>
-                <a href="/admin/news" class="btn btn-dark">Atpakaļ</a>
-                <button type="submit" class="btn btn-success">Pievienot</button>
-              </form>
+<x-admin-layout>
+  <x-slot name="header">
+    <h2 class="text-center">Pievienot jaunu aktualitāti</h2>
+  </x-slot>
+  <x-slot name="content">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-7 col-12">
+          @include('includes.status-messages')
+          <form action="/admin/news" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="alert alert-secondary mx-0 my-2" role="alert">
+              <p>Jaunums, ko pievienosi būs pieejams tikai {{ strtoupper(Lang::locale()) }}
+                valodā.</p>
             </div>
-          </div>
+            <div class="mb-3">
+              <label for="news-title" class="form-label">Nosaukums</label>
+              <input type="text" class="form-control" id="news-title"
+                     value="{{ old('news-title') }}"
+                     name="news-title">
+            </div>
+            <div class="mb-3">
+              <label for="news-content" class="form-label">Apraksts</label>
+              <x-description-text-area
+                :name="'news-content'">{{ old('news-content') }}</x-description-text-area>
+            </div>
+            <div class="row mb-3">
+              <div class="col">
+                <label for="news-images" class="form-label">Bildes</label>
+                <x-file-upload :name="'news-images'" :required="true"/>
+              </div>
+              <div class="col">
+                <label for="news-attachments" class="form-label">Pielikumi</label>
+                <x-file-upload :name="'news-attachments'"/>
+              </div>
+              <p class="small">Bildei ir jābūt .JPG, .JPEG vai .PNG formātā un pēc iespējas mazākā
+                izmērā.</p>
+              <p class="small">Tās var samazināt šajā lapā - <a href="https://compressor.io/"
+                                                                target="_blank">compressor.io</a>
+              <p class="small">Pielikumam ir jābūt .PDF un pēc iespējas mazākā izmērā.</p>
+            </div>
+            <a href="/admin/news" class="btn btn-dark">Atpakaļ</a>
+            <button type="submit" class="btn btn-success">Pievienot</button>
+          </form>
         </div>
       </div>
-    </section>
-  </div>
-@endsection
+    </div>
+  </x-slot>
+</x-admin-layout>
--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -1,41 +1,35 @@
-@extends('layouts.app')
-@section('content')
-  <div class="container">
-    @include('includes.admin-navbar')
-    <section>
-      <div class="mt-5">
-        <h2 class="text-center">Aktualitātes</h2>
-      </div>
-      <div>
-        <div class="container">
-          <div class="row justify-content-between">
-            <div class="mb-2">
-              <a href="/admin/news/create" class="btn btn-success">Pievienot jaunu</a>
-            </div>
-            @include('includes.status-messages')
-            @foreach($allNews as $news)
-              <div class="col-lg-4 p-2">
-                <div class="card position-relative">
-                  <div class="card-body">
-                    <p class="card-title text-center">{{ $news->title }}</p>
-                    <hr>
-                    <div class="d-flex justify-content-center mt-4">
-                      <a href="/admin/news/{{ $news->id }}/edit"
-                         class="btn btn-dark m-1">Rediģēt</a>
-                      <button type="button" data-bs-toggle="modal"
-                              data-bs-target="#delete-news-modal-{{$news->id}}"
-                              class="btn btn-danger m-1">
-                        Dzēst
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                @include('admin.news.delete-modal')
-              </div>
-            @endforeach
-          </div>
+<x-admin-layout>
+  <x-slot name="header">
+    <h2 class="text-center">Aktualitātes</h2>
+  </x-slot>
+  <x-slot name="content">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="mb-2">
+          <a href="/admin/news/create" class="btn btn-success">Pievienot jaunu</a>
         </div>
+        @include('includes.status-messages')
+        @foreach($allNews as $news)
+          <div class="col-lg-4 p-2">
+            <div class="card position-relative">
+              <div class="card-body">
+                <p class="card-title text-center">{{ $news->title }}</p>
+                <hr>
+                <div class="d-flex justify-content-center mt-4">
+                  <a href="/admin/news/{{ $news->id }}/edit"
+                     class="btn btn-dark m-1">Rediģēt</a>
+                  <button type="button" data-bs-toggle="modal"
+                          data-bs-target="#delete-news-modal-{{$news->id}}"
+                          class="btn btn-danger m-1">
+                    Dzēst
+                  </button>
+                </div>
+              </div>
+            </div>
+            @include('admin.news.delete-modal')
+          </div>
+        @endforeach
       </div>
-    </section>
-  </div>
-@endsection
+    </div>
+  </x-slot>
+</x-admin-layout>
