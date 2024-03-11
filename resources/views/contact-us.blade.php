f80b4427f91ee8150d943debf581a49e645f5316
Linards <9407839+llinards@users.noreply.github.com>
2024-03-11 20:54:51 +0200

    Decrease margin for title

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -5,7 +5,7 @@
     <div class="row my-4">
       <div class="col-lg-8 d-flex flex-column justify-content-center">
         <div>
-          <h3 class="title mb-4">@lang('contact us title 1')</h3>
+          <h3 class="title mb-2">@lang('contact us title 1')</h3>
           <p>@lang('contact us content 1')</p>
         </div>
       </div>
