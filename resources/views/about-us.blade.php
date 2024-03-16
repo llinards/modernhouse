aabbb2d2f78759eec4800cd1a7a97ac95cd5cce0
Linards <9407839+llinards@users.noreply.github.com>
2024-03-16 10:35:01 +0200

    Update layout for about us

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,51 +1,47 @@
-@extends('layouts.app', ['title' => Lang::get('about'), 'index' => false])
-@section('content')
-  <div class="container-xxl mb-4">
+<x-layouts.app :title="Lang::get('about')">
+  <x-slot name="header">
+    <h1 class="text-center text-uppercase title">@lang('about')</h1>
+  </x-slot>
+  <x-slot name="content">
     <div class="row">
-      <h1 class="fw-bold text-center text-uppercase title">@lang('about')</h1>
-      <div class="mt-4">
-        <div class="row">
-          <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column">
-            <img class="img-fluid" src="{{ asset('storage/about-us/one.jpg') }}"/>
-          </div>
-          <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-4">
-            <h2 class="fw-bold title mb-2">@lang('about us title 1 1')</h2>
-            <p class="pb-2">@lang('about us content 1 1')</p>
-            <p class="pb-2">@lang('about us content 1 2')</p>
-            <p>@lang('about us content 1 3')</p>
-          </div>
-        </div>
-        <div class="row mt-5">
-          <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-4">
-            <h2 class="fw-bold title mb-2">@lang('about us title 2 1')</h2>
-            <p>@lang('about us content 2 1')</p>
-          </div>
-          <div class="col-lg-6 d-flex order-first order-lg-last justify-content-center align-items-center flex-column">
-            <img class="img-fluid" src="{{ asset('storage/about-us/two.jpg') }}"/>
-          </div>
-        </div>
-        <div class="row mt-5">
-          <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column">
-            <img class="img-fluid" src="{{ asset('storage/about-us/three.jpg') }}"/>
-          </div>
-          <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-4">
-            <h2 class="fw-bold title mb-2">@lang('about us title 3 1')</h2>
-            <p>@lang('about us content 3 1')</p>
-            <p>@lang('about us content 3 2')</p>
-          </div>
-        </div>
-        <div class="row mt-5">
-          <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column ceo-quot">
-            <h5 class="fw-bold mb-2">@lang('about us title 4 1')</h5>
-            <p>Helvijs Ervalds</p>
-            <p>Modern House CEO</p>
-          </div>
-          <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column mt-lg-0 mt-4">
-            <img class="img-fluid" src="{{ asset('storage/about-us/four.jpg') }}"/>
-          </div>
-        </div>
+      <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column">
+        <img class="img-fluid" src="{{ asset('storage/about-us/one.jpg') }}"/>
+      </div>
+      <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-4">
+        <h2 class="fw-bold title mb-2">@lang('about us title 1 1')</h2>
+        <p class="pb-2">@lang('about us content 1 1')</p>
+        <p class="pb-2">@lang('about us content 1 2')</p>
+        <p>@lang('about us content 1 3')</p>
       </div>
     </div>
-  </div>
-  @include('includes.footer')
-@endsection
+    <div class="row mt-5">
+      <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-4">
+        <h2 class="fw-bold title mb-2">@lang('about us title 2 1')</h2>
+        <p>@lang('about us content 2 1')</p>
+      </div>
+      <div class="col-lg-6 d-flex order-first order-lg-last justify-content-center align-items-center flex-column">
+        <img class="img-fluid" src="{{ asset('storage/about-us/two.jpg') }}"/>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column">
+        <img class="img-fluid" src="{{ asset('storage/about-us/three.jpg') }}"/>
+      </div>
+      <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-4">
+        <h2 class="fw-bold title mb-2">@lang('about us title 3 1')</h2>
+        <p>@lang('about us content 3 1')</p>
+        <p>@lang('about us content 3 2')</p>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-lg-6 d-flex justify-content-center align-items-start flex-column ceo-quot">
+        <h5 class="fw-bold mb-2">@lang('about us title 4 1')</h5>
+        <p>Helvijs Ervalds</p>
+        <p>Modern House CEO</p>
+      </div>
+      <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column mt-lg-0 mt-4">
+        <img class="img-fluid" src="{{ asset('storage/about-us/four.jpg') }}"/>
+      </div>
+    </div>
+  </x-slot>
+</x-layouts.app>
