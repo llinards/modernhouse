facc5a23a3e08a9484e85b792b459f02ce9cc322
Linards <9407839+llinards@users.noreply.github.com>
2024-03-20 14:45:20 +0200

    Update last container with the background image

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -48,24 +48,30 @@
         </div>
       </div>
       <div class="row mb-4 flex-lg-row flex-column mx-auto">
-        <div class="col-lg-5 col-12 d-flex align-items-center justify-content-center interior-concept-box py-lg-5 pt-5">
-          <h3 class="title text-white">@lang('about us title five')</h3>
-        </div>
-        <div class="col-lg-7 col-12 interior-concept-box p-lg-5 py-5">
-          <p class="mb-2 text-white">@lang('about us content five one')</p>
-          <p class="text-white">@lang('about us content five two')</p>
+        <div
+          class="col-12 container-with-image-background p-lg-5 py-5 d-flex flex-column justify-content-center align-items-center"
+          style="background-image:linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url({{ asset('storage/about-us/4.jpg')}})">
+          <div class="row">
+            <div
+              class="col-lg-5 col-12 d-flex align-items-center justify-content-center py-lg-5 pt-5">
+              <h3 class="title text-white">@lang('about us title five')</h3>
+            </div>
+            <div class="col-lg-7 col-12 p-lg-5 py-5">
+              <p class="mb-2 text-white">@lang('about us content five one')</p>
+              <p class="text-white">@lang('about us content five two')</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-lg-6">
-        <div class="mb-2">
-          <h2 class="title text-center">@lang('apply to consultation and learn more')</h2>
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="mb-2">
+            <h2 class="title text-center">@lang('apply to consultation and learn more')</h2>
+          </div>
+          <x-contact-us-form :formId="'form_submit_about_us'" :hideCompanyField="false"
+                             :subject="'Jauna ziņa no mājaslapas (par mums sadaļas)'"/>
         </div>
-        <x-contact-us-form :formId="'form_submit_about_us'" :hideCompanyField="false"
-                           :subject="'Jauna ziņa no mājaslapas (par mums sadaļas)'"/>
       </div>
-    </div>
     </div>
   </x-slot>
 </x-layouts.app>
