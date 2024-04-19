d548ff7c17031e69aa643f1153824c318c1c2863
Linards <9407839+llinards@users.noreply.github.com>
2024-04-19 14:44:54 +0300

    Language check for landing page
    Add option to visit landing page in case language is LV

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -13,15 +13,18 @@ class LandingPageController extends Controller
 
   public function sviresIelasProjektsSigulda($language)
   {
-    $productVariantId = 29;
-    $productVariant = ProductVariant::select('id', 'slug')
-      ->with([
-        'productVariantOptions' => function ($query) {
-          $query->select('product_variant_id', 'option_title', 'option_category', 'options')
-            ->where('language', app()->getLocale());
-        }
-      ])
-      ->findOrFail($productVariantId);
-    return view('landing-pages.svires-ielas-projekts-sigulda', compact('productVariant'));
+    if ($language === 'lv') {
+      $productVariantId = 29;
+      $productVariant = ProductVariant::select('id', 'slug')
+        ->with([
+          'productVariantOptions' => function ($query) {
+            $query->select('product_variant_id', 'option_title', 'option_category', 'options')
+              ->where('language', app()->getLocale());
+          }
+        ])
+        ->findOrFail($productVariantId);
+      return view('landing-pages.svires-ielas-projekts-sigulda', compact('productVariant'));
+    }
+    abort(404);
   }
 }
