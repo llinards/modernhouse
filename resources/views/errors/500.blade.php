7e2c174afdb121f020007dfdec3a97466dc2185b
Linards <9407839+llinards@users.noreply.github.com>
2024-04-23 14:20:28 +0300

    Add CTA buttons for error pages

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,4 +1,5 @@
 <x-layouts.error>
   <x-slot name="title">500 | Modern House</x-slot>
   <h2 class="text-center text-uppercase">@lang('500 error')</h2>
+  <button class="btn btn-primary d-block mx-auto mt-4" onclick="window.history.back()">@lang('back')</button>
 </x-layouts.error>
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,4 +2,5 @@
   <x-slot name="title">503 | Modern House</x-slot>
   <h2 class="text-center text-uppercase">@lang('maintenance')</h2>
   <h3 class="text-center">@lang('be right back')</h3>
+  <button class="btn btn-primary d-block mx-auto mt-4" onclick="location.reload()">@lang('refresh')</button>
 </x-layouts.error>
