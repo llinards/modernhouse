2aecc669e02a0cb1c5387e6af563f3d2f1ed33a4
Linards <9407839+llinards@users.noreply.github.com>
2024-04-24 20:10:43 +0300

    Extract registration to the seperate component

--- app/Livewire/OpenDaysRegistration.php
+++ app/Livewire/OpenDaysRegistration.php
@@ -2,67 +2,23 @@
 
 namespace App\Livewire;
 
-use App\Http\Services\KlaviyoService;
-use App\Mail\CustomerRegisteredForOpenDays;
-use Illuminate\Support\Facades\Lang;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
-use Livewire\Attributes\Validate;
+use Livewire\Attributes\On;
 use Livewire\Component;
-use Spatie\Honeypot\Http\Livewire\Concerns\HoneypotData;
-use Spatie\Honeypot\Http\Livewire\Concerns\UsesSpamProtection;
 
 class OpenDaysRegistration extends Component
 {
-  use UsesSpamProtection;
-
-  public HoneypotData $extraFields;
-
   public bool $introductionView = true;
   public bool $registerView = false;
   public bool $successView = false;
 
-  #[Validate('required', message: 'Vārds ir obligāts.')]
-  #[Validate('string', message: 'Vārds drīkst sastāvēt tikai no burtiem.')]
-  #[Validate('max:255', message: 'Vārds ir aizdomīgi garš.')]
-  #[Validate('alpha', message: 'Vārds drīkst sastāvēt tikai no burtiem.')]
-  public string $firstName = '';
-
-  #[Validate('required', message: 'Uzvārds ir obligāts.')]
-  #[Validate('string', message: 'Uzvārds drīkst sastāvēt tikai no burtiem.')]
-  #[Validate('max:255', message: 'Uzvārds ir aizdomīgi garš.')]
-  #[Validate('alpha', message: 'Uzvārds drīkst sastāvēt tikai no burtiem.')]
-  public string $lastName = '';
-
-  #[Validate('required', message: 'Datums ir obligāts.')]
-  #[Validate('in:1.jūlijs,2.jūlijs', message: 'Pieteikties var tikai 1.jūlijā vai 2.jūlijā.')]
   public string $date = '';
-
-  #[Validate('required', message: 'Laiks ir obligāts.')]
-  #[Validate('in:10:00,11:00,12:00,13:00,14:00,15:00,16:00,17:00,18:00,19:00', message: 'Pieteikties var tikai uz pilnu stundu.')]
   public string $time = '';
-
-  #[Validate('required', message: 'Telefona numurs ir obligāts.')]
-  #[Validate('max:255', message: 'Telefona numurs ir aizdomīgi garš.')]
-  #[Validate('phone', message: 'Telefona numurs jānorāda ar valsts un/vai reģiona kodu. Piemēram, +371 12345678.')]
-  #[Validate('min:8', message: 'Telefona numurs nedrīkst būt īsāks par 8 cipariem.')]
-  public string $phoneNumber = '';
-
-  #[Validate('required', message: 'E-pasts ir obligāts.')]
-  #[Validate('email', message: 'E-pasts nav derīgs.')]
-  public string $email = '';
-
-  public string $questions = '';
-
-  #[Validate('accepted', message: 'Jums ir jāpiekrīt datu apstrādei un uzglabāšanai, lai reģistrētos.')]
-  public bool $consentToProcessPersonalData = false;
 
   public function mount(string $register = ''): void
   {
     if ($register) {
       $this->showRegisterView();
     }
-    $this->extraFields = new HoneypotData();
   }
 
   public function showRegisterView(): void
@@ -71,26 +27,13 @@ class OpenDaysRegistration extends Component
     $this->registerView = true;
   }
 
-  public function register(KlaviyoService $klaviyoService): void
+  #[On('registration-successful')]
+  public function showSuccessView(string $date, string $time): void
   {
-    $this->protectAgainstSpam();
-    $this->validate();
-    try {
-      Mail::to('info@modern-house.lv')->send(new CustomerRegisteredForOpenDays($this->all()));
-//      TODO: This should be fixed
-      $request = [
-        'email' => $this->email,
-        'phone-number' => $this->phoneNumber,
-        'first-name' => $this->firstName,
-        'last-name' => $this->lastName,
-      ];
-//      $klaviyoService->storeProfile($request, config('klaviyo.list_id_register_open_days'));
-      $this->registerView = false;
-      $this->successView = true;
-    } catch (\Exception $e) {
-      Log::error($e);
-      session()->flash('error', Lang::get('message has not been sent'));
-    }
+    $this->date = $date;
+    $this->time = $time;
+    $this->registerView = false;
+    $this->successView = true;
   }
 
   public function render()
