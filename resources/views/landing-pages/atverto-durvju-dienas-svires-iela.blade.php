5f53745ea06cbc475b3a7b77c94e3bac713c7ca2
Linards <9407839+llinards@users.noreply.github.com>
2024-04-28 17:18:33 +0300

    First version of landing page

--- resources/views/landing-pages/atverto-durvju-dienas-svires-iela.blade.php
+++ resources/views/landing-pages/atverto-durvju-dienas-svires-iela.blade.php
@@ -8,16 +8,16 @@
       <div class="text-center d-flex flex-column align-items-center">
         <div class="d-flex justify-content-center align-items-center flex-wrap">
           <div class="d-flex mb-3 mb-sm-4 mx-4">
-            <img class="icon" src="{{asset('storage/icons/calendar-check.svg')}}" alt="Calendar icon">
+            <img class="icon" src="{{asset('storage/icons/calendar-check-white.svg')}}" alt="Calendar icon">
             <p class="text-white fw-bold">1. un 2. jūlijs</p>
           </div>
           <div class="d-flex mb-3 mb-sm-4 mx-4">
-            <img class="icon" src="{{asset('storage/icons/clock.svg')}}" alt="Clock icon">
+            <img class="icon" src="{{asset('storage/icons/clock-white.svg')}}" alt="Clock icon">
             <p class="text-white fw-bold">10:00 - 19:00</p>
           </div>
           <div class="d-flex mb-3 mb-sm-4 mx-4">
             <img class="icon" src="{{asset('storage/landing-pages/icons/map.svg')}}" alt="Location icon">
-            <p class="text-white fw-bold">Sigulda, Svīres iela</p>
+            <p class="text-white fw-bold">Svīres iela, Sigulda</p>
           </div>
         </div>
         <div>
@@ -56,21 +56,21 @@
           <div class="col-lg-4 offset-lg-2 d-md-flex d-none flex-column justify-content-center mt-lg-0 mt-3">
             <div class="row">
               <div class="col-12 d-flex justify-content-lg-start justify-content-center">
-                <img class="icons" src="{{asset('storage/icons/calendar-check.svg')}}"
+                <img class="icons open-days-icons" src="{{asset('storage/icons/calendar-check.svg')}}"
                      alt="Calendar icon">
                 <h3>1. un 2. jūlijs</h3>
               </div>
             </div>
             <div class="row mt-4">
               <div class="col-12 d-flex justify-content-lg-start justify-content-center">
-                <img class="icons" src="{{asset('storage/icons/clock.svg')}}"
+                <img class="icons open-days-icons" src="{{asset('storage/icons/clock.svg')}}"
                      alt="Clock icon">
                 <h3>10:00 - 19:00</h3>
               </div>
             </div>
             <div class="row mt-4">
               <div class="col-12 d-flex justify-content-lg-start justify-content-center">
-                <img class="icons"
+                <img class="icons open-days-icons"
                      src="{{asset('storage/icons/map-pin-line-3.svg')}}"
                      alt="Location icon">
                 <h3>Svīres iela, Sigulda</h3>
@@ -87,22 +87,24 @@
     </section>
     {{--    About project (mobile only)--}}
     <section class="full-height-section d-md-none d-block">
-      <div class="container-xxl h-100 d-flex flex-column justify-content-center align-items-start">
-        <div class="d-flex align-items-center">
-          <img class="icons" src="{{asset('storage/icons/calendar-check.svg')}}"
-               alt="Calendar icon">
-          <h3>1. un 2. jūlijs</h3>
-        </div>
-        <div class="d-flex align-items-center mt-4">
-          <img class="icons" src="{{asset('storage/icons/clock.svg')}}"
-               alt="Clock icon">
-          <h3>10:00 - 19:00</h3>
-        </div>
-        <div class="d-flex align-items-center mt-4">
-          <img class="icons"
-               src="{{asset('storage/icons/map-pin-line-3.svg')}}"
-               alt="Location icon">
-          <h3>Svīres iela, Sigulda</h3>
+      <div class="container-xxl h-100 d-flex flex-column justify-content-center">
+        <div class="mx-auto">
+          <div class="d-flex align-items-center">
+            <img class="icons open-days-icons" src="{{asset('storage/icons/calendar-check.svg')}}"
+                 alt="Calendar icon">
+            <h3>1. un 2. jūlijs</h3>
+          </div>
+          <div class="d-flex align-items-center mt-2">
+            <img class="icons open-days-icons" src="{{asset('storage/icons/clock.svg')}}"
+                 alt="Clock icon">
+            <h3>10:00 - 19:00</h3>
+          </div>
+          <div class="d-flex align-items-center mt-2">
+            <img class="icons open-days-icons"
+                 src="{{asset('storage/icons/map-pin-line-3.svg')}}"
+                 alt="Location icon">
+            <h3>Svīres iela, Sigulda</h3>
+          </div>
         </div>
         <div class="mt-4 align-self-center">
           <div class="d-flex">
@@ -116,38 +118,58 @@
     {{--    Partnership--}}
     <section class="full-height-section">
       <div class="container-xxl h-100 d-flex flex-column justify-content-center">
-        <div class="row">
-          <h2 class="mb-4 text-center">Māju finansējuma<br/>partneris</h2>
+        <div class="row mb-4">
+          <h2 class="mb-4 text-center">Finansētāji</h2>
           <div class="col-lg-12 d-flex flex-column justify-content-evenly">
-            <div>
-              <div class="text-center">
-                <img class="swedbank-logo mb-2" src="{{asset('storage/landing-pages/icons/swedbank-logo.svg')}}"
+            <div class="row financiers-logo">
+              <div class="col-6 text-center">
+                <img class="swedbank-logo mb-2" src="{{asset('storage/icons/swedbank-logo.svg')}}"
                      alt="Swedbank logo">
               </div>
-              <p class="mb-4 text-center">Saņem konsultāciju par mājas finansējuma iespējām, aizpildot pieteikumu ar
-                kodu
-                "MODERNHOUSE".</p>
-              <div class="d-flex justify-content-center">
-                <a href="https://www.swedbank.lv/private/credit/loans/home?campaignCode=MODERNHOUSE_1&language=LAT"
-                   target="_blank" id="apply-consultation-swedbank"
-                   class="btn btn-primary d-flex justify-content-center align-items-center"
-                >Pieteikties konsultācijai</a>
+              <div class="col-6 text-center">
+                <img class="seb-logo mb-2" src="{{asset('storage/icons/seb-logo.jpg')}}"
+                     alt="SEB logo">
               </div>
             </div>
           </div>
-          <div class="col-lg-6 d-none flex-column justify-content-evenly mt-lg-0 mt-4">
-            <div>
-              <div class="text-center">
-                <img class="seb-logo mb-3" src="{{asset('storage/landing-pages/icons/seb-logo.jpg')}}"
-                     alt="SEB bank logo">
+        </div>
+        <div class="row">
+          <h2 class="mb-4 text-center">Sadarbībā ar</h2>
+          <div class="col-lg-12 d-flex flex-column justify-content-evenly">
+            <div class="row mb-4 partnership-logo">
+              <div class="col-6 col-md-3 text-center">
+                <img class="mb-2" src="{{asset('storage/logo/logo-black.png')}}"
+                     alt="">
               </div>
-              <p class="mb-3 text-center">Saņem konsultāciju par mājas finansējuma iespējām, aizpildot pieteikumu ar
-                kodu
-                "MODERNHOUSE".</p>
-              <div class="d-flex justify-content-center">
-                <a href="https://www.seb.lv/" target="_blank"
-                   class="btn btn-primary fw-light d-flex justify-content-center align-items-center"
-                >Pieteikties konsultācijai</a>
+              <div class="col-6 col-md-3 text-center">
+                <img class="mb-2" src="{{asset('storage/logo/logo-black.png')}}"
+                     alt="">
+              </div>
+              <div class="col-6 col-md-3 text-center">
+                <img class="mb-2" src="{{asset('storage/logo/logo-black.png')}}"
+                     alt="">
+              </div>
+              <div class="col-6 col-md-3 text-center">
+                <img class="mb-2" src="{{asset('storage/logo/logo-black.png')}}"
+                     alt="">
+              </div>
+            </div>
+            <div class="row partnership-logo">
+              <div class="col-6 col-md-3 text-center">
+                <img class="mb-2" src="{{asset('storage/logo/logo-black.png')}}"
+                     alt="">
+              </div>
+              <div class="col-6 col-md-3 text-center">
+                <img class="mb-2" src="{{asset('storage/logo/logo-black.png')}}"
+                     alt="">
+              </div>
+              <div class="col-6 col-md-3 text-center">
+                <img class="mb-2" src="{{asset('storage/logo/logo-black.png')}}"
+                     alt="">
+              </div>
+              <div class="col-6 col-md-3 text-center">
+                <img class="mb-2" src="{{asset('storage/logo/logo-black.png')}}"
+                     alt="">
               </div>
             </div>
           </div>
@@ -155,36 +177,9 @@
       </div>
     </section>
     {{--    Explore neighbourhood--}}
-    <section class="full-height-section d-flex flex-column">
-      <h2 class="mb-4 pt-3 text-center">Iepazīsti apkaimi</h2>
-      <div class="h-100 mb-3">
+    <section class="full-height-section d-flex flex-column justify-content-center">
+      <div class="h-75 mb-3">
         <iframe src="https://snazzymaps.com/embed/564387" class="map-size"></iframe>
-      </div>
-      <div class="container-xxl mb-5">
-        <div class="row justify-content-evenly">
-          <div class="col-lg-3 col-6 mb-lg-0 mb-3 text-center">
-            <h3>1,8 km</h3>
-            <p class="fw-bold">Pirmsskolas izglītības iestāde "Saulīte"</p>
-          </div>
-          <div class="col-lg-3 col-6 mb-lg-0 mb-3 text-center">
-            <h3>600 m</h3>
-            <p class="fw-bold">Siguldas Sporta skola</p>
-          </div>
-          <div class="col-lg-3 col-6 text-center">
-            <h3>2,4 km</h3>
-            <p class="fw-bold">Siguldas Valsts ģimnāzija</p>
-          </div>
-          <div class="col-lg-3 col-6 text-center">
-            <h3>2,1 km</h3>
-            <p class="fw-bold">pilsētas centrs</p>
-          </div>
-          <div>
-            <hr class="m-3">
-          </div>
-          <a href="#contact-us"
-             class="btn btn-primary d-flex justify-content-center align-items-center"
-          >Pieteikties konsultācijai</a>
-        </div>
       </div>
     </section>
     {{--    Full project house--}}
@@ -192,21 +187,15 @@
       <div class="container-xxl h-100 d-flex flex-column justify-content-center">
         <div class="row">
           <div class="col-lg-5 d-flex flex-column justify-content-evenly">
-            <h2 class="mb-2">No pamatiem<br/>līdz atslēgai</h2>
-            <div>
-              <p class="mb-2">Energoefektīva koka karkasa vienstāvīga ģimenes māja ar terasi, noliktavu un auto
-                novietni
-                divām automašīnām. Mājā ir plānotas trīs guļamistabas, plaša dzīvojamā telpa, kas ir apvienota ar
-                virtuvi un augstajiem griestiem līdz jumta konstrukcijai, pārējā mājas daļā griestu augstums 2,70m.
-                Pārējās telpas – vannas istaba un dušas telpa ar tualeti, kā arī tehniskā telpa.</p>
-              <p class="mb-2"><strong>MĀJAI BŪS PILNĪBĀ PABEIGTI ĀRDARBI</strong><br/>
-                Ārējā apdare, labiekārtota teritorija, uzstādīta sēta un automātiskie iebraucamie vārti.</p>
-              <p class="mb-2"><strong>MĀJAI BŪS PILNĪBĀ PABEIGTI IEKŠDARBI</strong><br/>
-                Krāsotas sienas, griesti, noflīzēti un aprīkoti sanmezgli, ieklāts grīdas segums, uzstādīta virtuves
-                iekārta ar virtuves tehniku, masīvkoka iekšdurvis, iebūvējamie gaismekļi sanmezglos un gaitenī.</p>
-              <p><strong>KOMUNIKĀCIJAS</strong><br/>
-                Elektrība, santehnika, rekuperācijas iekārta, gaiss - ūdens siltumsūknis ar siltajām grīdām.</p>
-            </div>
+            <h2 class="mb-2">Atvērto durvju dienu programma</h2>
+            <ul class="open-days-agenda">
+              <li class="mb-2">Sarunas ar MODERN HOUSE komandu</li>
+              <li class="mb-2">Sarunas ar arhitektu,inženieri, projektu vadītāju, pārdošanas vadītāju</li>
+              <li class="mb-2">Sarunas ar SEB un Swedbank pārstāvjiem</li>
+              <li class="mb-2">Sarunas ar MODERN HOUSE klientiem</li>
+              <li class="mb-2">Sarunas ar sadarbības partneriem</li>
+              <li>Iespēja noslēgt līgumu par labāku cenu</li>
+            </ul>
           </div>
           <div class="col-lg-7 d-lg-flex d-none flex-column justify-content-center">
             <img loading="lazy" class="full-container-image"
