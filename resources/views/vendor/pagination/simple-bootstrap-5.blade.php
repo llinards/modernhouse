52226237d8f3c0d4c10e67e590a6a43b182f15fc
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-07-01 21:28:01 +0300

    Add icons for the button

--- resources/views/vendor/pagination/simple-bootstrap-5.blade.php
+++ resources/views/vendor/pagination/simple-bootstrap-5.blade.php
@@ -4,14 +4,14 @@
       @if ($paginator->hasMorePages())
         <li class="page-item mb-2 mb-md-0 order-1 order-md-2">
           <a class="btn btn-primary d-flex justify-content-center align-items-center "
-             href="{{ $paginator->nextPageUrl() }}" rel="next">{!! __('next') !!}</a>
+             href="{{ $paginator->nextPageUrl() }}" rel="next">{!! __('next') !!} &#8594;</a>
         </li>
       @endif
       @if (!$paginator->onFirstPage())
         <li class="page-item order-2 order-md-1">
           <a class="btn btn-primary d-flex justify-content-center align-items-center "
              href="{{ $paginator->previousPageUrl() }}" rel="prev">
-            {!! __('back') !!}
+            &#8592; {!! __('back') !!}
           </a>
         </li>
       @endif
