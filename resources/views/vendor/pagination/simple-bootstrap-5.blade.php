217e8e209d2f8b55247c3df13ebd9544963887f6
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-07-02 15:45:46 +0300

    Add new type of buttons

--- resources/views/vendor/pagination/simple-bootstrap-5.blade.php
+++ resources/views/vendor/pagination/simple-bootstrap-5.blade.php
@@ -1,15 +1,15 @@
 @if ($paginator->hasPages())
   <nav role="navigation" aria-label="Pagina tion Navigation">
-    <ul class="pagination mt-4 mb-0 d-flex justify-content-center flex-wrap gap-0 gap-md-2">
+    <ul class="pagination mt-4 mb-0 d-flex justify-content-center flex-wrap gap-0 gap-sm-2">
       @if ($paginator->hasMorePages())
-        <li class="page-item mb-2 mb-md-0 order-1 order-md-2">
-          <a class="btn btn-primary d-flex justify-content-center align-items-center "
+        <li class="page-item mb-2 mb-md-0 order-1 order-sm-2">
+          <a class="btn btn-primary btn-primary-sm d-flex justify-content-center align-items-center "
              href="{{ $paginator->nextPageUrl() }}" rel="next">{!! __('next') !!} &#8594;</a>
         </li>
       @endif
       @if (!$paginator->onFirstPage())
-        <li class="page-item order-2 order-md-1">
-          <a class="btn btn-primary d-flex justify-content-center align-items-center "
+        <li class="page-item order-2 order-sm-1">
+          <a class="btn btn-primary btn-primary-sm d-flex justify-content-center align-items-center "
              href="{{ $paginator->previousPageUrl() }}" rel="prev">
             &#8592; {!! __('back') !!}
           </a>
