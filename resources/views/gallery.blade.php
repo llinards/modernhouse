dff793553e9b1a9b6bc6a5ec2b9f1088562bdf53
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-12-25 18:17:35 +0200

    Update the columns for gallery view

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -5,13 +5,13 @@
   <x-slot name="content">
     @foreach($galleries as $gallery)
       <div class="row mt-4 gallery-item">
-        <div class="col-lg-3 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-2">
+        <div class="col-lg-4 d-flex justify-content-center align-items-start flex-column mt-lg-0 mt-2">
           <h2 class="mb-2">{{ $gallery->translations[0]->title }}</h2>
           <p>{!! $gallery->translations[0]->content !!}</p>
         </div>
         @if($gallery->is_video)
           <div
-            class="col-lg-9 d-flex order-first order-lg-last justify-content-center align-items-center flex-column">
+            class="col-lg-8 d-flex order-first order-lg-last justify-content-center align-items-center flex-column">
             <video class="img-fluid" controls
                    src="{{ asset('storage/gallery/'. $gallery->slug . '/' .$gallery->images[0]->filename) }}#t=0.001"
                    title="{{ $gallery->translations[0]->title }}">Your browser does not support the
@@ -20,7 +20,7 @@
           </div>
         @else
           <div
-            class="col-lg-9 d-flex order-first order-lg-last justify-content-center align-items-center flex-column"
+            class="col-lg-8 d-flex order-first order-lg-last justify-content-center align-items-center flex-column"
             id="gallery-images">
             <section id="gallery-{{$gallery->slug}}-main-carousel" class="splide">
               <div class="splide__track">
