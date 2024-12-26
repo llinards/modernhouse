459968d20ab16caaf58ad47c6329c90edc500c3c
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-12-26 23:13:10 +0200

    Remove language check for adding gallery

--- resources/views/admin/gallery/index.blade.php
+++ resources/views/admin/gallery/index.blade.php
@@ -4,11 +4,9 @@
   </x-slot>
   <x-slot name="content">
     <div class="row justify-content-between">
-      @if(app()->getLocale() == 'lv')
-        <div class="my-2">
-          <a href="/admin/gallery/create" class="btn btn-success">Pievienot jaunu</a>
-        </div>
-      @endif
+      <div class="my-2">
+        <a href="/admin/gallery/create" class="btn btn-success">Pievienot jaunu</a>
+      </div>
       <livewire:admin.gallery-list/>
     </div>
   </x-slot>
