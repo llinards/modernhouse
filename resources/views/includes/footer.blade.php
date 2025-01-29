b625bdd97793e8ccb6fcf00443a25ff7a5edecc0
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-01-29 17:50:21 +0200

    Update footer phone links with WhatsApp

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -11,13 +11,17 @@
       </div>
       <div class="col-md-2 col-6">
         <p class="small">
-          <i class="bi bi-envelope-fill text-white"></i> info@modern-house.ie
+          <i class="bi bi-envelope-fill text-white"></i> <a href="mailto:info@modern-house.ie"
+                                                            class="text-white text-decoration-none">info@modern-house.ie</a>
         </p>
         <p class="small">
-          <i class="bi bi-telephone text-white"></i> +353 83 480 1610
+          <i class="bi bi-telephone text-white"></i> <a href="https://wa.link/2ojact" target="_blank"
+                                                        class="text-white text-decoration-none">+353 83 480 1610</a>
         </p>
         <p class="small">
-          <i class="bi bi-telephone text-white"></i> +353 83 854 9155
+          <i class="bi bi-telephone text-white"></i> <a href="https://wa.link/by73e7" target="_blank"
+                                                        class="text-white text-decoration-none">+353
+            83 854 9155</a>
         </p>
       </div>
       <div class="col-md-1 col-12 d-flex flex-md-column justify-content-center align-items-md-end">
