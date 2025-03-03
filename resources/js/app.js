78f4df9014ace0b979d027879eeac4a15e760301
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-03-03 15:50:36 +0200

    Add slider for product variant titles

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,15 @@ import * as bootstrap from 'bootstrap'
 import L from 'leaflet';
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import {Fancybox} from "@fancyapps/ui";
+import Swiper from 'swiper';
+import {Navigation} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 
 window.Splide = Splide;
 window.bootstrap = bootstrap;
 window.L = L;
 window.Fancybox = Fancybox;
+window.Swiper = Swiper;
+window.Navigation = Navigation;
