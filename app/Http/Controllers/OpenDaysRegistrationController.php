79fbc4ee58083eeb1b7ea00873448fd9e6001416
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-03-28 07:34:38 +0200

    Add option to delete all submissions

--- app/Http/Controllers/OpenDaysRegistrationController.php
+++ app/Http/Controllers/OpenDaysRegistrationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Exports\OpenDaysRegistrationExport;
 use App\Models\OpenDaysRegistration;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Facades\Excel;
 
 class OpenDaysRegistrationController extends Controller
@@ -11,17 +13,41 @@ class OpenDaysRegistrationController extends Controller
   public function index()
   {
     $submissions = OpenDaysRegistration::orderByDateTime()->get();
+
     return view('admin.open-days-registration', compact('submissions'));
   }
 
-  public function destroy(OpenDaysRegistration $openDaysRegistration)
+  public function destroyOne(OpenDaysRegistration $openDaysRegistration)
   {
-    $openDaysRegistration->delete();
-    return redirect()->back();
+    try {
+      $openDaysRegistration->delete();
+
+      return back()->with('success', 'Pieteikums izdzēsts!');
+    } catch (\Exception $e) {
+      Log::error($e);
+
+      return redirect()->back()->with('error', 'Kļūda! Mēģini vēlreiz.');
+    }
   }
 
   public function export()
   {
     return Excel::download(new OpenDaysRegistrationExport, 'atverto-durvju-dienas-pieteikumi-'.date('d-m-Y').'.xlsx');
+  }
+
+  public function destroy(Request $data)
+  {
+    try {
+      $allSubmissions = OpenDaysRegistration::all();
+      foreach ($allSubmissions as $submission) {
+        $submission->delete();
+      }
+
+      return back()->with('success', 'Pieteikumi izdzēsti!');
+    } catch (\Exception $e) {
+      Log::error($e);
+
+      return redirect()->back()->with('error', 'Kļūda! Mēģini vēlreiz.');
+    }
   }
 }
