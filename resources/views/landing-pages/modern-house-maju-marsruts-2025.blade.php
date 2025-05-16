1a031deed9d363b42d2158f59daa513fee6f6280
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-05-16 09:29:34 +0300

    feat: integrate `intl-tel-input` library for phone input enhancements
    - Add `intl-tel-input` dependency to `package.json`
    - Enable phone input enhancements in registration form
    - Update mix assets to include changes

--- resources/views/landing-pages/modern-house-maju-marsruts-2025.blade.php
+++ resources/views/landing-pages/modern-house-maju-marsruts-2025.blade.php
@@ -151,7 +151,7 @@
             </div>
         </section>
         <section class="mt-4 mt-lg-5">
-            
+
         </section>
         <section id="open-days-registration" class="mt-4 mt-lg-5"></section>
         <section id="contact-us" class="mt-4 mt-lg-5">
