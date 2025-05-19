a79c4daac30f28f14054d0db7b803c5ddf0ceb2b
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-05-19 21:33:01 +0300

    feat: add "Modern House Furniture" section
    - Introduced a new route and view for "Modern House Furniture".
    - Updated navbar to include link for the new section.
    - Added localized strings and translations for related content.
    - Created new styles and assets for "Modern House Furniture".
    - Added CEO quote component to the new section.

---
+++ resources/views/components/ceo-quot-mh-furniture.blade.php
@@ -0,0 +1,8 @@
+<div>
+  <p class="quot-element">“</p>
+  <div class="mx-4">
+    <h3 class="text-uppercase mb-4">@lang('ceo quot mh furniture')</h3>
+    <p class="text-uppercase">@lang('helvijs')</p>
+    <p class="text-uppercase">@lang('ceo')</p>
+  </div>
+</div>
