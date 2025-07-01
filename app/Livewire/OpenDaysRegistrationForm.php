74d322256dd9a654dd4e421586ca82463eae4028
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-07-01 07:32:48 +0300

    chore: comment out mail sending in Open Days registration
    - Temporarily disable mail sending logic for `CustomerRegisteredForOpenDays`.

--- app/Livewire/OpenDaysRegistrationForm.php
+++ app/Livewire/OpenDaysRegistrationForm.php
@@ -3,11 +3,9 @@
 namespace App\Livewire;
 
 use App\Http\Services\KlaviyoService;
-use App\Mail\CustomerRegisteredForOpenDays;
 use App\Models\OpenDaysRegistration;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Spatie\Honeypot\Http\Livewire\Concerns\HoneypotData;
@@ -65,7 +63,7 @@ class OpenDaysRegistrationForm extends Component
     $this->validate();
     try {
       OpenDaysRegistration::create($this->all());
-      Mail::to('info@modern-house.lv')->send(new CustomerRegisteredForOpenDays($this->all()));
+//      Mail::to('info@modern-house.lv')->send(new CustomerRegisteredForOpenDays($this->all()));
       $request = [
         'email'        => $this->email,
         'phone-number' => $this->phoneNumber,
