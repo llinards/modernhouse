08019cde813752a52b2ede9b0dac141aec22ea97
Linards Lazdiņš <linards_lazdins@hotmail.com>
2026-04-09 18:39:33 +0300

    style: fix formatting and remove required attribute from create product form
    - Adjusted HTML formatting for better readability.
    - Removed `required` attributes from form fields to handle validation dynamically.

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -5,17 +5,18 @@
   <x-slot name="content">
     <div class="row justify-content-center">
       <div class="col-lg-7 col-12">
-        <x-status-messages />
-        <form action="{{ route('admin.products.store', ['locale' => app()->getLocale()]) }}" method="POST" enctype="multipart/form-data">
+        <x-status-messages/>
+        <form action="{{ route('admin.products.store', ['locale' => app()->getLocale()]) }}" method="POST"
+              enctype="multipart/form-data">
           @csrf
           <div class="mb-3">
             <label for="product-name" class="form-label">Nosaukums</label>
-            <input type="text" required="required" class="form-control" id="product-name" name="product-name">
+            <input type="text" class="form-control" id="product-name" name="product-name">
           </div>
           <div class="mb-3">
             <label for="product-cover-photo" class="form-label">Produkta pirmās lapas
               bilde</label>
-            <x-file-upload :name="'product-cover-photo'" :required="'true'"/>
+            <x-file-upload :name="'product-cover-photo'"/>
             <p class="small">Bildei ir jābūt .JPG, .JPEG vai .PNG formātā un pēc iespējas mazākā
               izmērā.</p>
             <p class="small">Tās var samazināt šajā lapā - <a href="https://compressor.io/"
@@ -29,7 +30,8 @@
             <p class="small">Video ir jābūt .MP4 formātā un pēc iespējas mazākā
               izmērā.</p>
           </div>
-          <a href="{{ route('admin.products.index', ['locale' => app()->getLocale()]) }}" class="btn btn-dark">Atpakaļ</a>
+          <a href="{{ route('admin.products.index', ['locale' => app()->getLocale()]) }}"
+             class="btn btn-dark">Atpakaļ</a>
           <button type="submit" class="btn btn-success">Pievienot</button>
         </form>
       </div>
